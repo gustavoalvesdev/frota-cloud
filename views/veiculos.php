3fb7376bc31a514f1557f8ad7a7ad620724fe501
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-03-13 17:49:28 -0300

    frontend de paginacao e buscas na listagem de veiculos

--- views/veiculos.php
+++ views/veiculos.php
@@ -1,9 +1,27 @@
 <div class="container">
 	<div class="jumbotron">
-		<h1 class="text-center">TODOS OS VEÍCULOS</h1>
+		<h2 class="text-center">TODOS OS VEÍCULOS</h2>
 		<!-- text-center -->
 	</div>
 	<!-- jumbotron -->
+
+	<form>
+		<div class="row"	>
+			<div class="col-md-12">
+				<div class="form-group">
+					<label for="busca_veiculo">Buscar por um veículo:</label>
+					<input type="search" name="busca_veiculo" class="form-control">
+					<!-- form-control -->
+					
+				</div>
+				<!-- form-group -->
+				<input type="submit" class="btn btn-primary btn-lg" name="btn_buscar" value="Pesquisar">
+				<!-- btn btn-primary btn-lg -->
+			</div>
+			<!-- col-md-12 -->
+		</div>
+		<!-- row -->
+	</form>
 
 	<div class="table-responsive">
 		<table class="table table-striped table-bordered">
@@ -47,5 +65,15 @@
 		<!-- table table-striped table-bordered -->
 	</div>
 	<!-- table-responsive -->
+
+	<ul class="pagination">
+		<li class="active"><a href="#">1</a></li>
+		<!-- active -->
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li><a href="#">4</a></li>
+		<li><a href="#">5</a></li>
+	</ul>
+	<!-- pagination -->
 </div>
 <!-- container -->
